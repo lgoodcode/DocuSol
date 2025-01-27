03e9c979f33f529d40dd52ceec6a67dcdf3d0b38
lgoodcode <lawrence.good55@gmail.com>
2025-01-26 20:12:43 -0800

    use dvh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-dvh bg-background font-sans antialiased",
           satoshiFont.variable
         )}
       >
