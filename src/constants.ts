be514de32386cd2a475547a30fcedf0309ba09c5
Lawrence Good <lawrence.good55@gmail.com>
2025-01-28 19:48:15 -0800

    Add CA and update home page text (#16)
    # Description
    
    Title

--- src/constants.ts
+++ src/constants.ts
@@ -10,7 +10,7 @@ export const IS_MOBILE = () => {
 
 export const GITHUB_URL = "https://github.com/DocuSol";
 
-export const CONTRACT_ADDRESS = "";
+export const CONTRACT_ADDRESS = "2thfFEH6nR7Qbqd315Dt9BjSJRau9YQ4mmBhkAuepump";
 export const X_URL = "https://x.com/docusol_";
 export const PUMPFUN_URL = "";
 export const DEXSCREENER_URL = "";
