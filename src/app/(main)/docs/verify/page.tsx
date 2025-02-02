7cba3f13d502f6ab75272b411f9f41293cd1dab0
lgoodcode <lawrence.good55@gmail.com>
2025-02-02 13:18:05 -0800

    remove notices

--- src/app/(main)/docs/verify/page.tsx
+++ src/app/(main)/docs/verify/page.tsx
@@ -1,10 +1,10 @@
-import { VerifyNoticeDialog } from "@/components/verify-notice-dialog";
+// import { VerifyNoticeDialog } from "@/components/verify-notice-dialog";
 import { VerifyDocContent } from "./verify-doc-content";
 
 export default function VerifyDocumentPage() {
   return (
     <>
-      <VerifyNoticeDialog />
+      {/* <VerifyNoticeDialog /> */}
       <div className="container max-w-3xl mx-auto py-8 space-y-8">
         <VerifyDocContent />
       </div>
