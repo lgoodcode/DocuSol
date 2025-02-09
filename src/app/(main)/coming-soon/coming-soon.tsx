d2948afeaebc84e35618ab9e007a5f29fc8e28a5
lgoodcode <lawrence.good55@gmail.com>
2025-02-09 12:38:53 -0800

    fix coming soon text

--- src/app/(main)/coming-soon/coming-soon.tsx
+++ src/app/(main)/coming-soon/coming-soon.tsx
@@ -4,15 +4,15 @@ import { motion } from "framer-motion";
 
 export function ComingSoon() {
   return (
-    <div className="relative overflow-hidden min-h-[calc(100dvh-200px)] flex items-center justify-center">
-      <div className="relative container mx-auto flex flex-col items-center justify-center max-w-4xl text-center gap-4 px-4">
+    <div className="relative flex min-h-[calc(100dvh-200px)] items-center justify-center overflow-hidden">
+      <div className="container relative mx-auto flex max-w-4xl flex-col items-center justify-center gap-4 px-4 text-center">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
           className="space-y-4"
         >
-          <h1 className="p-2 text-4xl sm:text-5xl md:text-6xl font-bold tracking-tighter bg-clip-text text-transparent bg-gradient-to-r from-primary to-primary/60 dark:to-primary/50">
+          <h1 className="bg-gradient-to-r from-primary to-primary/60 bg-clip-text p-2 text-4xl font-bold tracking-tighter text-transparent dark:to-primary/50 sm:text-5xl md:text-6xl">
             Coming Soon
           </h1>
         </motion.div>
@@ -22,9 +22,9 @@ export function ComingSoon() {
           transition={{ delay: 0.2, duration: 0.5 }}
           className="w-full max-w-sm space-y-8"
         >
-          <p className="sm:text-lg md:text-xl text-muted-foreground mx-auto">
+          <p className="mx-auto text-muted-foreground sm:text-lg md:text-xl">
             We are working hard to bring you the future of decentralized
-            document signing powered by Solana and DeepSeek-R1.
+            document signing.
           </p>
         </motion.div>
       </div>
