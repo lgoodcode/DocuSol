fb32caea7352acdca365bdc7f1b1b67c01d640dd
lgoodcode <lawrence.good55@gmail.com>
2025-02-17 09:41:48 -0800

    remove constant path

--- src/constants.ts
+++ src/constants.ts
@@ -16,8 +16,6 @@ export const IS_MOBILE = () => {
 
 export const PLATFORM_FEE = 100.0001;
 
-export const AUTHENTICATED_REDIRECT_TO = "/docs/list";
-
 /** mime_type: [extensions] */
 export const ACCEPTED_FILE_TYPES: Record<string, string[]> = {
   "application/pdf": [".pdf"],
